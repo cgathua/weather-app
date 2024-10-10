3aed5cbd341ed3be597e0e27187f11a603a6334a
cgathua <114153640+cgathua@users.noreply.github.com>
2024-10-10 13:06:35 -0400

    Update main.ts
    separate functions, use typescript object types  and add comments.

--- src/main.ts
+++ src/main.ts
@@ -36,6 +36,81 @@ const dayTwoMin = document.querySelector('[data-name="daytwomin"]') as HTMLDataE
 
 const loadingScreen = document.querySelector('[data-name="spinner"]') as HTMLDivElement;
 
+type CurrentConditions = {
+  currentConditions: {
+    cloudcover: number,
+    datetime: string,
+    humidity: number,
+    feelslike: number,
+    sunrise: string,
+    sunset: string
+  }
+}
+
+function renderConditions(json: CurrentConditions): void {
+  cloudCover.textContent = `${Math.round(json.currentConditions.cloudcover)}%`;
+  dateTime.textContent = json.currentConditions.datetime;
+  humidity.textContent = `${Math.round(json.currentConditions.humidity)}°`;
+  feelsLike.textContent = `${Math.round(json.currentConditions.feelslike)}°`;
+  sunrise.textContent = json.currentConditions.sunrise;
+  sunset.textContent = json.currentConditions.sunset;
+}
+
+type RenderForecast = {
+  days: [
+    {
+      icon: string,
+      conditions: string,
+      description: string,
+      datetime: string,
+      feelslikemax: number,
+      feelslikemin: number
+    },
+    {
+      icon: string,
+      conditions: string,
+      description: string,
+      datetime: string,
+      feelslikemax: number,
+      feelslikemin: number
+    },
+    {
+      icon: string,
+      conditions: string,
+      description: string,
+      datetime: string,
+      feelslikemax: number,
+      feelslikemin: number
+    }
+  ]
+}
+
+function renderForecast(json: RenderForecast): void {
+  // today's weather forecast
+  dayZeroIcon.src = `icons/${json.days[0].icon}.svg`;
+  dayZeroConditions.textContent = json.days[0].conditions;
+  dayZeroDescription.textContent = json.days[0].description;
+  dayZero.textContent = json.days[0].datetime;
+  dayZeroMax.textContent = `${Math.round(json.days[0].feelslikemax)}° F`;
+  dayZeroMin.textContent = `${Math.round(json.days[0].feelslikemin)}° F`;
+
+  // tomorrow's weather forecast
+  dayOneIcon.src = `icons/${json.days[1].icon}.svg`;
+  dayOneConditions.textContent = json.days[1].conditions;
+  dayOneDescription.textContent = json.days[1].description;
+  dayOne.textContent = json.days[1].datetime;
+  dayOneMax.textContent = `${Math.round(json.days[1].feelslikemax)}° F`;
+  dayOneMin.textContent = `${Math.round(json.days[1].feelslikemin)}° F`;
+
+  // day after tomorrow forecast
+  dayTwoIcon.src = `icons/${json.days[2].icon}.svg`;
+  dayTwoConditions.textContent = json.days[2].conditions;
+  dayTwoDescription.textContent = json.days[2].description;
+  dayTwo.textContent = json.days[2].datetime;
+  dayTwoMax.textContent = `${Math.round(json.days[2].feelslikemax)}° F`;
+  dayTwoMin.textContent = `${Math.round(json.days[2].feelslikemin)}° F`;
+}
+
 const API_KEY: string = 'AYNDWPDRMYWBLPWRNX7XZQW79'
 async function searchAPI(): Promise<void> {
   loadingScreen.classList.remove('hidden');
@@ -47,40 +122,19 @@ async function searchAPI(): Promise<void> {
       throw new Error(`HTTP Error! status: ${response.statusText}`)
 
     } else {
+      // get data
       const json = await response.json();
+      // log data to the browser console
       console.log(json);
-
-      cloudCover.textContent = `${Math.round(json.currentConditions.cloudcover)}%`;
-      dateTime.textContent = json.currentConditions.datetime;
-      humidity.textContent = `${Math.round(json.currentConditions.humidity)}°`;
-      feelsLike.textContent = `${Math.round(json.currentConditions.feelslike)}°`;
-      sunrise.textContent = json.currentConditions.sunrise;
-      sunset.textContent = json.currentConditions.sunset;
+      // user input location or default's to Atlanta, on page load
       resolvedAddress.textContent = json.resolvedAddress;
+      // current conditions description
       description.textContent = json.description;
+      // current conditions icon
       iconDisplay.src = `icons/${json.currentConditions.icon}.svg`;
-
-      dayZeroIcon.src = `icons/${json.days[0].icon}.svg`;
-      dayZeroConditions.textContent = json.days[0].conditions;
-      dayZeroDescription.textContent = json.days[0].description;
-      dayZero.textContent = json.days[0].datetime;
-      dayZeroMax.textContent = `${Math.round(json.days[0].feelslikemax)}° F`;
-      dayZeroMin.textContent = `${Math.round(json.days[0].feelslikemin)}° F`;
-
-      dayOneIcon.src = `icons/${json.days[1].icon}.svg`;
-      dayOneConditions.textContent = json.days[1].conditions;
-      dayOneDescription.textContent = json.days[1].description;
-      dayOne.textContent = json.days[1].datetime;
-      dayOneMax.textContent = `${Math.round(json.days[1].feelslikemax)}° F`;
-      dayOneMin.textContent = `${Math.round(json.days[1].feelslikemin)}° F`;
-
-      dayTwoIcon.src = `icons/${json.days[2].icon}.svg`;
-      dayTwoConditions.textContent = json.days[2].conditions;
-      dayTwoDescription.textContent = json.days[2].description;
-      dayTwo.textContent = json.days[2].datetime;
-      dayTwoMax.textContent = `${Math.round(json.days[2].feelslikemax)}° F`;
-      dayTwoMin.textContent = `${Math.round(json.days[2].feelslikemin)}° F`;
-
+      // invoke above functions
+      renderConditions(json)
+      renderForecast(json);
     }
   } catch (error) {
     console.error('Error retreiving weather data:', error);
@@ -94,9 +148,10 @@ async function searchAPI(): Promise<void> {
 searchAPI();
 
 form.addEventListener('submit', (Event: SubmitEvent) => {
+  // on submit, prevent the page from refreshing
   Event.preventDefault();
+  // if userInput() has a value, invoke searchAPI()
   if (userInput) {
     searchAPI()
   }
-
 });
